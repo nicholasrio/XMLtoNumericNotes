4ed009fe723eb59c19a1c2f738721c151ccddc85
Nicholas Rio <nicholasrio2811@gmail.com>
2013-06-17 17:28:37 +0700

    Adding paragraph formatting

--- index.php
+++ index.php
@@ -6,13 +6,16 @@
 	$huruf = array('name'=>'aahuruf','size'=>10);
 	$angka = array('name'=>'aaangka','size'=>10);
 	$symbol = array('name'=>'aasymbol','size'=>10);
+	$par = array('spacing'=>1, 'spaceBefore'=>0, 'spaceAfter'=>0);
 	$PHPWord->addFontStyle('garis',$garis);
 	$PHPWord->addFontStyle('huruf',$huruf);
 	$PHPWord->addFontStyle('angka',$angka);
 	$PHPWord->addFontStyle('symbol',$symbol);
+	$PHPWord->addParagraphStyle('par',$par);
 	//Done adding style
 	$section = $PHPWord->createSection();
-	$section->addText('Hello World','huruf');
+	$section->addText('Hello World','huruf','par');
+	$section->addText('Ini cuma coba-coba','huruf','par');
 	$objWriter = PHPWord_IOFactory::createWriter($PHPWord, 'Word2007');
 	$objWriter->save('helloWorld.docx');
 ?>
